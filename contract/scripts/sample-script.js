f56f2a4be9cfd97c7459b1893063b27b08cd8bfa
Chirag <56735482+Chirag018@users.noreply.github.com>
2022-05-28 12:10:22 +0530

    initial commit

---
+++ contract/scripts/sample-script.js
@@ -0,0 +1,35 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  
+  [signer1, signer2, signer3] = await ethers.getSigners();
+
+  const Donation = await hre.ethers.getContractFactory("Donation", signer1);
+  const donation = await Donation.deploy();
+
+  console.log('Donation contract deployed to :', donation.address, "by", signer1.address);
+
+  await signer1.sendTransaction({
+    to: donation.address,
+    value: ethers.utils.parseUnits('0.1', 18)
+  });
+  await signer2.sendTransaction({
+    to: donation.address,
+    value: ethers.utils.parseUnits('0.1', 18)
+  });
+  await signer3.sendTransaction({
+    to: donation.address,
+    value: ethers.utils.parseUnits('2', 18)
+  });
+
+  // console.log("Greeter deployed to:", greeter.address);
+}
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
